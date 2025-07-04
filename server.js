3613e47b4a61cacf17ad951cc5320f7dfc3869bc
krishna123-png <khirani787@gmail.com>
2025-07-05 00:52:02 +0530

    updated backend

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: 'https://razorpay-demo-frontend.vercel.app/',
+        origin: 'https://razorpay-demo-frontend.vercel.app',
         credentials: true
     }
 ));
